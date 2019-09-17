e56b9bd1108c7efc8d04686c1cbd8c281a85e55c
Andrew West <alwest@partners.org>
2019-09-17 11:48:09 -0400

    more cleanup

--- lib/model/tracerVisit.dart
+++ lib/model/tracerVisit.dart
@@ -1,21 +1,21 @@
+import 'package:Tracer/model/observation.dart';
 import 'package:Tracer/model/place.dart';
 import 'package:Tracer/model/template/template.dart';
 import 'package:json_annotation/json_annotation.dart';
-import 'observation.dart';
 
 part 'tracerVisit.g.dart';
 
 @JsonSerializable()
 class TracerVisit extends Object with _$TracerVisitSerializerMixin {
-
   String id;
   Place place;
   String summary;
+  String todo;  
   String participants;
-  String todo;
   DateTime visitDatetime;
   SummaryStats summaryStats;
   String completionStatus;
+  String type;
 
   @JsonKey(name: 'observationCategories')
   Map<String, Observation> observations;
@@ -24,20 +24,21 @@ class TracerVisit extends Object with _$TracerVisitSerializerMixin {
   
   @JsonKey(name: 'template_version')
   String templateVersion;
-  String type;
 
   TracerVisit(
     this.id,
     this.place,
     this.summary,
-    this.summaryStats,
     this.todo,
     this.participants,
+    this.visitDatetime,
+    this.summaryStats,
     this.completionStatus,
+    this.type,
     this.observations,
     this.exceptions,
-    this.templateVersion,
-    this.type);
+    this.templateVersion
+    );
 
   factory TracerVisit.fromJson(Map<String, dynamic> json) =>
       _$TracerVisitFromJson(json);
--- lib/model/tracerVisit.g.dart
+++ lib/model/tracerVisit.g.dart
@@ -13,13 +13,17 @@ TracerVisit _$TracerVisitFromJson(Map<String, dynamic> json) {
           ? null
           : new Place.fromJson(json['place'] as Map<String, dynamic>),
       json['summary'] as String,
+      json['todo'] as String,
+      json['participants'] as String,
+      json['visitDatetime'] == null
+          ? null
+          : DateTime.parse(json['visitDatetime'] as String),
       json['summaryStats'] == null
           ? null
           : new SummaryStats.fromJson(
               json['summaryStats'] as Map<String, dynamic>),
-      json['todo'] as String,
-      json['participants'] as String,
       json['completionStatus'] as String,
+      json['type'] as String,
       (json['observationCategories'] as Map<String, dynamic>)?.map((k, e) =>
           new MapEntry(
               k,
@@ -31,39 +35,35 @@ TracerVisit _$TracerVisitFromJson(Map<String, dynamic> json) {
           e == null
               ? null
               : new ObservationException.fromJson(e as Map<String, dynamic>))),
-      json['template_version'] as String,
-      json['type'] as String)
-    ..visitDatetime = json['visitDatetime'] == null
-        ? null
-        : DateTime.parse(json['visitDatetime'] as String);
+      json['template_version'] as String);
 }
 
 abstract class _$TracerVisitSerializerMixin {
   String get id;
   Place get place;
   String get summary;
-  String get participants;
   String get todo;
+  String get participants;
   DateTime get visitDatetime;
   SummaryStats get summaryStats;
   String get completionStatus;
+  String get type;
   Map<String, Observation> get observations;
   Map<String, ObservationException> get exceptions;
   String get templateVersion;
-  String get type;
   Map<String, dynamic> toJson() => <String, dynamic>{
         'id': id,
         'place': place,
         'summary': summary,
-        'participants': participants,
         'todo': todo,
+        'participants': participants,
         'visitDatetime': visitDatetime?.toIso8601String(),
         'summaryStats': summaryStats,
         'completionStatus': completionStatus,
+        'type': type,
         'observationCategories': observations,
         'exceptions': exceptions,
-        'template_version': templateVersion,
-        'type': type
+        'template_version': templateVersion
       };
 }
 
